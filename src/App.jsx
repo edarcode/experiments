3b10aa28a3d049ed73bf37aebc49a4ff33816fb5
Edwin Ortiz <edarcode@gmail.com>
2022-06-09 14:44:48 -0500

    scroll snap type

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { posters } from "./data/posters";
 
 function App() {
-	return <div className="App">edarcode</div>;
+	return (
+		<div className="App">
+			{posters.map(({ img, title }) => (
+				<img key={title} src={img} alt="rick" />
+			))}
+		</div>
+	);
 }
 
 export default App;
